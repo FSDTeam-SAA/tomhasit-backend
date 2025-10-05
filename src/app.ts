9f4a2e2408f2941d6d2fe91ee9f90bbb55b6371b
Sajjad177 <sajjadhossainx06@gmail.com>
2025-10-05 07:02:53 +0600

    cors updated

--- src/app.ts
+++ src/app.ts
@@ -13,8 +13,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-  // origin: "*",
-  origin: ["http://localhost:3000", "http://localhost:3001", "*"],
+  origin: [
+    "https://providingdialtone.com",
+  ],
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
   credentials: true,
 };
